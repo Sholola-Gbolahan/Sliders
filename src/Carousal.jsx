eeaf7a3c9d9029f2f83342b49ca201a70b49025b
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-06-15 22:01:06 -0700

    Css styling for sliding content was done

--- src/Carousal.jsx
+++ src/Carousal.jsx
@@ -4,7 +4,7 @@ import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousal = () => {
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(list);
 
   const prevSlide = () =>{
 
@@ -16,15 +16,15 @@ const Carousal = () => {
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person ,personIndex) => {
         const { id, image, name, quote, title } = person;
 
         return (
-          <article className="slide" key={id}>
+          <article className="slide "  style={{transform:`translateX(${100 * personIndex }%)`}} key={id}>
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
-            <p className="quote">{quote}</p>
+            <p className="text">{quote}</p>
             <FaQuoteRight className="icon" />
           </article>
         );
