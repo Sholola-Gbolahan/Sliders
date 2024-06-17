6b9433204eef342509e627a72de2b45f44bdb734
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-06-17 14:42:25 -0700

    slide func and auto slide func. done

--- src/Carousal.jsx
+++ src/Carousal.jsx
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { shortList, longList, list } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousal = () => {
   const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(2)
+
+  const checkNumber = (number) => {
+    if( number > people.length -1){
+      return 0
+    }
+
+    if( number < 0){
+      return people.length -1 
+    }
+
+    return number;
+
+  }
 
   const prevSlide = () =>{
 
+    setCurrentPerson((oldPerson)=>{
+      const result = oldPerson - 1
+      return checkNumber(result)
+
+    })
   }
 
   const nextSlide = () =>{
+
+    setCurrentPerson((oldPerson)=>{
+      const result = oldPerson + 1
+      return checkNumber(result)
+
+    })
     
   }
+
+  useEffect(()=>{
+
+    const slideId = setInterval(()=>{
+      nextSlide()
+    },2000)
+ return () => {
+  clearInterval(slideId)
+ }
+
+  },[currentPerson])
 
   return (
     <section className="slider-container">
@@ -20,7 +56,11 @@ const Carousal = () => {
         const { id, image, name, quote, title } = person;
 
         return (
-          <article className="slide "  style={{transform:`translateX(${100 * personIndex }%)`}} key={id}>
+          <article className="slide "  style={{transform:`translateX(${100 *( personIndex - currentPerson )}%)`,
+          opacity: personIndex === currentPerson ? 1 : 0,
+          visibility: personIndex === currentPerson ? 'visible' : 'hidden'
+          
+          }} key={id}>
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
